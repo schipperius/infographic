874e3c64f4d77b61ba4eb9db3390aee58d84e85a
Mark Schipper <schipperius@gmail.com>
2013-12-20 15:24:24 -0700

    update gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Infographic::Application.config.secret_key_base = 'fa9eb08cf4f0e8427d0284078c3dad5586e91dacd0f1a01aa0277821f2465c68f54fd03dea1b5271be040d1d07e7de6981744041c410a44bcf36a06a98f8b1de'
+# Be sure to restart your server when you modify this file.# Your secret key is used for verifying the integrity of signed cookies.# If you change this key, all old signed cookies will become invalid!# Make sure the secret is at least 30 characters and all random,# no regular words or you'll be exposed to dictionary attacks.# You can use `rake secret` to generate a secure secret key.# Make sure your secret_key_base is kept private# if you're sharing your code publicly.require 'securerandom'def secure_token  token_file = Rails.root.join('.secret')  if File.exist?(token_file)    # Use the existing token.    File.read(token_file).chomp  else    # Generate a new token and store it in token_file.    token = SecureRandom.hex(64)    File.write(token_file, token)    token  endendInfographic::Application.config.secret_key_base = secure_token
