da675030c6125948be1146759c4dc2fbc86fe08a
Mark Schipper <schipperius@gmail.com>
2013-12-20 14:04:10 -0700

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Infographic::Application.config.secret_key_base = 'fa9eb08cf4f0e8427d0284078c3dad5586e91dacd0f1a01aa0277821f2465c68f54fd03dea1b5271be040d1d07e7de6981744041c410a44bcf36a06a98f8b1de'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Infographic::Application.config.session_store :cookie_store, key: '_infographic_session'
